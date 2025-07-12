ae2cf9056b3b35090f251df068fc0b47f93aecea
sodiqscript111 <simidusodiqt@gmail.com>
2025-07-12 14:22:31 +0100

    Fixixng erro

--- src/components/offer.tsx
+++ src/components/offer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState, createContext, useContext } from "react";
+import  { useEffect, useState, createContext, useContext } from "react";
 import { IconArrowNarrowLeft, IconArrowNarrowRight } from "@tabler/icons-react";
 import { cn } from "../lib/utils";
 import { AnimatePresence, motion } from "framer-motion";
--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo, useState } from "react";
+import  { useMemo, useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "../lib/utils";
 
